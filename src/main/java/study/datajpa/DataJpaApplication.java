3eeb80e231db0b42cf139ec40c5ad39024f07897
Dayeong <dayeongmaeng@gmail.com>
2023-03-31 17:58:51 +0900

    feat: Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,13 +1,24 @@
 package study.datajpa;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        //실무에서는 SecurityContextHolder 등에서 사용자 객체를 꺼내야 함
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
