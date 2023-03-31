a3d38c0fa8a1ddbdb6073fc2d4ce9bf52e9930f6
Dayeong <dayeongmaeng@gmail.com>
2023-03-31 17:33:17 +0900

    feat: 사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -272,4 +272,9 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
